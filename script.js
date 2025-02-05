897b767145910c7c24b1cf69d792ccd293bc227e
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-05 16:24:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here!
+const list = document.querySelector("#infi-list");
 
+list.innerHTML = `
+<li>Item 1</li>
+<li>Item 1</li>
+<li>Item 1</li>
+<li>Item 1</li>
+<li>Item 1</li>
+<li>Item 1</li>
+<li>Item 1</li>
+<li>Item 1</li>
+<li>Item 1</li>
+<li>Item 1</li>
+`
