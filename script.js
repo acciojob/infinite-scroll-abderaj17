c6ede966a36dd9c2c40fa0b95001521d8bf7cb5d
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-05 17:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-//your code here!
 const list = document.querySelector("#infi-list");
 
 for(let i = 1; i <= 10; i++){
 	list.innerHTML += `<li>Item ${i}</li>`
-	
 }
+
+let count = 10;
+let observer = new IntersectionObserver(entries =>{
+	if(entries[0].isIntersecting){
+		for(let i = 1; i <= 2; i++){
+			count++;
+			list.innerHTML += `<li>Item ${count}</li>`;
+		}
+		observer.unobserve(lastItem);
+		lastItem = list.lastElementChild;
+		observer.observe(lastItem);
+	}
+},{
+	threshold: 1.0
+});
+
+let lastItem = list.lastElementChild;
+observer.observe(lastItem);
